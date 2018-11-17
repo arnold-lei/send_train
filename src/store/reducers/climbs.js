4c7cae5d97e607c576a63682232e8b21dc81266c
arnold-lei <arnold.lei88@gmail.com>
2018-11-17 02:47:31 -0500

    adding redux

---
+++ src/store/reducers/climbs.js
@@ -0,0 +1,14 @@
+const initialState = {
+    climbs: [],
+    selectedClimb: null,
+}; 
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    
+    default: 
+      return state; 
+  }
+}
+
+export default reducer; 
