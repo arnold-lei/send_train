e674d09559bd96d873305d4f2ca3873beb454ed7
arnold-lei <arnold.lei88@gmail.com>
2018-11-25 23:49:14 -0500

    added maps to android

--- android/app/src/main/java/com/send_train/MainApplication.java
+++ android/app/src/main/java/com/send_train/MainApplication.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativenavigation.NavigationApplication;
-
+import com.airbnb.android.react.maps.MapsPackage;
 
 public class MainApplication extends NavigationApplication {
 
@@ -57,7 +57,9 @@ public class MainApplication extends NavigationApplication {
 		// Add additional packages you require here
 		// No need to add RnnPackage and MainReactPackage
 		return Arrays.<ReactPackage>asList(
-      new VectorIconsPackage()
+      new VectorIconsPackage(),
+      new MainReactPackage(),
+      new MapsPackage()
 		);
 	}
 
